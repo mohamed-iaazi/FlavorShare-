b69f5d142e4c3cb95cee51924c603e9ab1cfd5c8
mohamed <iaazi.mohamed.1998@gmail.com>
2025-05-01 20:43:47 +0100

    create the login form with ngmodel

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,26 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import {NgClass} from '@angular/common';
+import {FormsModule} from '@angular/forms';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, NgClass, FormsModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
 export class AppComponent {
   title = 'FlavorShare';
+  isMenuOpen: boolean=false;
+  email: any;
+  password: any;
+
+
+  toggleMenu() {
+    this.isMenuOpen = !this.isMenuOpen;
+  }
+
+  login() {
+
+  }
 }
