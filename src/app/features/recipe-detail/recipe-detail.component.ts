eb38f944edbb70dfb1c5ff5b49c1bab60c6ae508
mohamed <iaazi.mohamed.1998@gmail.com>
2025-05-02 14:26:35 +0100

    display meal details

--- src/app/features/recipe-detail/recipe-detail.component.ts
+++ src/app/features/recipe-detail/recipe-detail.component.ts
@@ -1,31 +1,42 @@
-import {Component, Input, OnInit} from '@angular/core';
-import {HttpClient} from '@angular/common/http';
-import {NgForOf} from '@angular/common';
+import { Component, Input, OnInit } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { NgForOf } from '@angular/common';
 
 @Component({
   selector: 'app-recipe-detail',
-  imports: [
-    NgForOf
-  ],
+  imports: [NgForOf],
   templateUrl: './recipe-detail.component.html',
-  styleUrl: './recipe-detail.component.css'
+  styleUrls: ['./recipe-detail.component.css']
 })
-export class RecipeDetailComponent  implements OnInit {
+export class RecipeDetailComponent implements OnInit {
   @Input() mealId!: string;
+  mealImage = '';
+  mealTitle = '';  // Now mealTitle will be fetched from API
+  instructions: string[] = [];
   ingredients: string[] = [];
 
   constructor(private http: HttpClient) {}
 
   ngOnInit(): void {
     if (this.mealId) {
-      this.http
-        .get<any>(`https://www.themealdb.com/api/json/v1/1/lookup.php?i=${this.mealId}`)
+      this.http.get<any>(`https://www.themealdb.com/api/json/v1/1/lookup.php?i=${this.mealId}`)
         .subscribe(res => {
-          const meal = res.meals[0];
-          for (let i = 1; i <= 20; i++) {
-            const ingredient = meal[`strIngredient${i}`];
-            if (ingredient) {
-              this.ingredients.push(ingredient);
+          const meal = res.meals?.[0];
+          if (meal) {
+            this.mealImage = meal.strMealThumb;
+            this.mealTitle = meal.strMeal;  // Set the meal title from the API response
+
+            // Break instructions into steps
+            this.instructions = meal.strInstructions
+              ?.split('. ')
+              .filter((step: string) => step.trim().length > 0)
+              .map((step: string) => step.trim() + '.');
+
+            // Collect ingredients
+            this.ingredients = [];
+            for (let i = 1; i <= 20; i++) {
+              const ing = meal[`strIngredient${i}`];
+              if (ing) this.ingredients.push(ing);
             }
           }
         });
